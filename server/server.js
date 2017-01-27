b40ac6b932fca7235a3d35b48ba71e4da4422764
Ossaija ThankGod <codebugsolved@gmail.com>
2017-01-27 13:11:47 +0100

    imported socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
+
 const publicPath = path.join(__dirname, ('../public'));
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+  console.log('new user connected to the server');
+  socket.on('disconnect', () => {
+    console.log('user has left');
+  })
+});
 
-app.listen(PORT, () =>{
+io.on('disconnect', () => {
+  console.log('user has disconnected');
+})
+
+server.listen(PORT, () =>{
   console.log(`app started on port ${PORT}`);
 })
