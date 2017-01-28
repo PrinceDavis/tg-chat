36417659c7228e14a5af28f181d92f4e125d303e
Ossaija ThankGod <codebugsolved@gmail.com>
2017-01-28 07:55:34 +0100

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log('new user connected to the server');
 
+  //welcome for admin
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Welcome to wechat',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'new user Has joined',
+    createAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (newMessage) => {
     console.log('incoming message', newMessage);
     io.emit('newMessage', {
       from: newMessage.from,
       text: newMessage.text,
       createAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
